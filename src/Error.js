cf78c91cdc6d0b72472ce1de4051037dde14a68d
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2026-01-25 03:23:54 +0200

    Update Error.js

--- src/Error.js
+++ src/Error.js
@@ -1,7 +1,7 @@
-function Error() {
+function Error({ message }) {
   return (
     <p className="error">
-      <span>💥</span> There was an error fecthing questions.
+      <span>💥</span> There was an error, {message}
     </p>
   );
 }
