b81ede076d66ec822718862cde20c68f8a87e537
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2026-01-25 04:54:40 +0200

    Update Options.jsx

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,8 +1,16 @@
-function Options({ question }) {
+function Options({ question, dispatch, answer, points }) {
+  const hasAnswered = answer !== null;
+
   return (
     <div className="options">
       {question.options.map((option, index) => (
-        <button className="btn btn-option" key={index}>
+        <button
+          className={`btn btn-option  ${index === answer ? "answer" : ""} 
+            ${hasAnswered ? (index === question.correctOption ? "correct" : "wrong") : ""}`}
+          key={option}
+          onClick={() => dispatch({ type: "ANSWER_QUESTION", payload: index })}
+          disabled={hasAnswered}
+        >
           {option}
         </button>
       ))}
