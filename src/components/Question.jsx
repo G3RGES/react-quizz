5dc35f5d737ea8bddda460ca2683a70b43d7fea0
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2026-01-25 04:54:41 +0200

    Update Question.jsx

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,11 +1,18 @@
 import Options from "./Options";
 
-function Question({ question }) {
+function Question({ question, dispatch, answer, points }) {
   return (
     <div>
       <h4>{question.question}</h4>
 
-      <Options question={question} />
+      <h4>Points: {points}</h4>
+
+      <Options
+        question={question}
+        dispatch={dispatch}
+        answer={answer}
+        points={points}
+      />
 
       <button>Answer</button>
       <button>Next Question</button>
