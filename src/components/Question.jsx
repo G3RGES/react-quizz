d902cb77e49893850b68df947392f60017931fcc
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2026-01-26 14:49:44 +0200

    Update Question.jsx

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -3,9 +3,9 @@ import Options from "./Options";
 function Question({ question, dispatch, answer, points }) {
   return (
     <div>
-      <h4>{question.question}</h4>
-
       <h4>Points: {points}</h4>
+
+      <h4>{question.question}</h4>
 
       <Options
         question={question}
@@ -13,11 +13,6 @@ function Question({ question, dispatch, answer, points }) {
         answer={answer}
         points={points}
       />
-
-      <button>Answer</button>
-      <button>Next Question</button>
-      <button>Finish Quiz</button>
-      <button>Restart Quiz</button>
     </div>
   );
 }
