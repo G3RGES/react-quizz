751b2c96d895b112d4016df4a9b04c38e93d5c5d
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2026-01-27 15:20:12 +0200

    Update FinishScreen.jsx

--- src/components/FinishScreen.jsx
+++ src/components/FinishScreen.jsx
@@ -1,4 +1,4 @@
-function FinishScreen({ points, highscore }) {
+function FinishScreen({ points, highscore, highestscore }) {
   const percentage = (points / highscore) * 100;
 
   let emoji;
@@ -14,10 +14,14 @@ function FinishScreen({ points, highscore }) {
   if (percentage === 0) emoji = "💩";
 
   return (
-    <p className="result">
-      You scored <strong>{points}</strong> out of {highscore} (
-      {Math.ceil(percentage)}%) {emoji}
-    </p>
+    <>
+      <p className="result">
+        You scored <strong>{points}</strong> out of {highscore} (
+        {Math.ceil(percentage)}%) <span>{emoji}</span>
+      </p>
+
+      <p className="highscore">(Highscore : {highestscore} points)</p>
+    </>
   );
 }
 
