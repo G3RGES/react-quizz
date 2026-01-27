54c60fae5ddfc47d12f55a428a3465a049493eca
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2026-01-27 15:08:17 +0200

    Update FinishScreen.jsx

--- src/components/FinishScreen.jsx
+++ src/components/FinishScreen.jsx
@@ -1,10 +1,22 @@
 function FinishScreen({ points, highscore }) {
   const percentage = (points / highscore) * 100;
 
+  let emoji;
+
+  if (percentage === 100) emoji = "🎖️";
+
+  if (percentage >= 80 && percentage < 100) emoji = "🏅";
+
+  if (percentage >= 50 && percentage < 80) emoji = "👍";
+
+  if (percentage >= 0 && percentage < 50) emoji = "👎";
+
+  if (percentage === 0) emoji = "💩";
+
   return (
-    <p>
+    <p className="result">
       You scored <strong>{points}</strong> out of {highscore} (
-      {Math.ceil(percentage)}%)
+      {Math.ceil(percentage)}%) {emoji}
     </p>
   );
 }
